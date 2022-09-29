03d20032ae5dfca77d093e3dff64f3f1e6aeed4b
zakumann <samuel742able@gmail.com>
2022-09-30 00:20:57 +0900

    first upload 09302022

---
+++ LearnReactNative/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Greeting from './components/Greeting';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Greeting />
+    </SafeAreaView>
+  );
+};
+
+export default App;
