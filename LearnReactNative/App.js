d71d998a6686c1b776e100f3a1bff01616f17259
zakumann <samuel742able@gmail.com>
2022-09-30 15:24:20 +0900

    Counter app

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greeting';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from'./components/Counter';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
   return (
-    <SafeAreaView>
-      <Greeting />
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
