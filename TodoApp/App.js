2f5da9ebd3878f844604ffef3535881d670f0bdd
zakumann <samuel742able@gmail.com>
2022-10-23 21:18:48 +0900

    TodoItem 컴포넌트 생성

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
-import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+
+import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 function App(){
   const today = new Date();
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
   
-  return(
+  return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
         <KeyboardAvoidingView 
-          behavior={Platform.OS ===' ios' ? 'padding' : undefined}
+          behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
           <AddTodo />
         </KeyboardAvoidingView>
       </SafeAreaView>
