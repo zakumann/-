b41a76f0bca2fefb8e4f6f1ac3ef1b38c3e98b7a
zakumann <samuel742able@gmail.com>
2022-10-24 15:01:10 +0900

    AsyncStorage 삭제 및 코드 수정

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-community/async-storage';
+import todosStorage from './storages/todosStorage';
 
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
@@ -10,23 +11,21 @@ import TodoList from './components/TodoList';
 
 function App(){
   const today = new Date();
-
   const [todos, setTodos] = useState([
     {id: 1, text: '작업환경 설정', done: true},
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
 
+  //불러오기
+  useEffect(()=> {
+    todosStorage
+      .get().then(setTodos).catch(console.error);
+  }, []);
+
   //저장
   useEffect(()=> {
-    async function save() {
-      try {
-        await AsyncStorage.setItem('todos', JSON.stringify(todos));
-      } catch (e){
-        console.log('Failed to save todos');
-      }
-    }
-    save();
+    todosStorage.set(todos).catch(console.error);
   }, [todos]);
   useEffect(()=> {
     console.log(todos);
