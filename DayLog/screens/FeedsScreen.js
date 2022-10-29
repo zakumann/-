a4be7a4da006616923e285a8ad7058f0ff900bb4
zakumann <samuel742able@gmail.com>
2022-10-30 00:56:19 +0900

    create Diary app

---
+++ DayLog/screens/FeedsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function FeedsScreen(){
+    return <View style={StyleSheet.block} />
+}
+
+const styles = StyleSheet.create({
+    block: {},
+});
+
+export default FeedsScreen;
---
+++ DayLog/screens/WriteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function WriteScreen(){
+    return <View style={styles.block} />
+}
+
+const style = StyleSheet.create({
+    block: {},
+});
+
+export default WriteScreen;
